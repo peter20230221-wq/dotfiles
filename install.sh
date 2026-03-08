128cbf063f080d7521f19a43eb7f8bf1fc5d4b90
peterLiu <peter20230221@gmail.com>
2026-03-08 17:00:13 +0900

    modify install.sh

--- install.sh
+++ install.sh
@@ -6,12 +6,17 @@ sudo pacman -Syu
 echo '安装基础编译依赖'
 sudo pacman -S --needed base-devel rust cargo openssl pkg-config
 echo '安装字体'
-echo '安装字体'
 sudo pacman -S --needed noto-fonts noto-fonts-cjk noto-fonts-emoji ttf-nerd-fonts-symbols ttf-jetbrains-mono-nerd
 echo '安装AUR管理器'
 sudo pacman -S --needed yay paru
 echo '安装hyprland及其组件'
 sudo pacman -S --needed hyprland hyprsunset hypridle hyprlock hyprpolkitagent hyprcursor
+echo '安装剪贴板管理器'
+sudo pacman -S --needed copyq
+echo '安装输入法'
+sudo pacman -S --needed fcitx5 fcitx5-chinese-addons fcitx5-configtool
+echo '安装网络管理托盘'
+sudo pacman -S --needed network-manager-applet
 echo '安装wofi软件启动器'
 sudo pacman -S --needed wofi
 echo '安装kitty终端模拟器'
