be3ce8fbf21e2a82077dd6dc107e5784acabed60
peterLiu <peter20230221@gmail.com>
2026-05-20 20:01:34 +0800

    add neovim's configuration

---
+++ .config/nvim/lua/config/options.lua
@@ -0,0 +1,10 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+vim.opt.guifont = "JetBrainsMono Nerd Font:h14"
+vim.opt.guifont = "Maple Mono NF CN:h14"
+vim.g.neovide_cursor_animation_length = 0.2
+vim.g.neovide_cursor_trail_size = 2
+vim.g.neovide_opacity = 0.9
+vim.g.neovide_normal_opacity = 0.9
+vim.g.neovide_scroll_animation_length = 0.15
